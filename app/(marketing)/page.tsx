f1efafc9934d82c8daf4f0108325fa021219844a
Scott Moss <willscottmoss@gmail.com>
2025-02-27 13:40:05 -0600

    chore

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -11,7 +11,7 @@ export default async function LandingPage() {
             <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-gray-900 dark:text-white tracking-tight">
               Issue tracking <br className="hidden sm:block" />
               <span className="text-purple-600 dark:text-purple-400">
-                simplified
+                simplified.
               </span>
             </h1>
             <p className="mt-6 max-w-2xl mx-auto text-lg sm:text-xl text-gray-600 dark:text-gray-300">
